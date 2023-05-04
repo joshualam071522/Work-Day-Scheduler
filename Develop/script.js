acb0c36e97ce2c958338db73d8fe844649cc507d
joshualam071522 <127908020+joshualam071522@users.noreply.github.com>
2023-05-04 15:35:07 -0700

    added document.ready function

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+//* Wrapped in document.ready function to ensure code does not run until html is loaded
+
+$(document).ready(function () {
   
   //* current time variable
-  
+
   var currentTime = dayjs();
 
   //* Event Listener for save button
@@ -13,38 +12,38 @@ $(function () {
   var hourNumber = $(this).parent().attr("id");
   var toDo = $(this).siblings('textArea').val().trim();
   localStorage.setItem(hourNumber, toDo);
- })
+  })
   
- //* function to change class of each timeblock to past, present, or future
- //* based on current time  
+  //* function to change class of each timeblock to past, present, or future
+  //* based on current time  
 
-$('.time-block').each(function() {
-  var hourNumberSplit = $(this).attr("id").split('-')[1];
+  $('.time-block').each(function() {
   
-  if (currentTime.hour() > hourNumberSplit) {
-    $(this).removeClass("future present")
-    $(this).addClass("past");
+    var hourNumberSplit = $(this).attr("id").split('-')[1];
+  
+    if (currentTime.hour() > hourNumberSplit) {
+      $(this).removeClass("future present")
+      $(this).addClass("past");
+      
+    } else if (currentTime.hour() == hourNumberSplit) {
+      $(this).removeClass("future past")
+      $(this).addClass("present");
     
-  } else if (currentTime.hour() == hourNumberSplit) {
-    $(this).removeClass("future past")
-    $(this).addClass("present");
-  
-  } else {
-    $(this).removeClass("past present")
-    $(this).addClass("future");
-  }
-})
-
+    } else {
+      $(this).removeClass("past present")
+      $(this).addClass("future");
+    }
+  })
 
   //* function to retrieve user input from local storage
 
-$('.time-block').each(function() {
-  var savedHourNumber = $(this).attr("id");
-  var savedToDo = localStorage.getItem(savedHourNumber);
-  if (savedToDo) {
-    $(this).children('textArea').val(savedToDo);
-  }
-})
+  $('.time-block').each(function() {
+    var savedHourNumber = $(this).attr("id");
+    var savedToDo = localStorage.getItem(savedHourNumber);
+    if (savedToDo) {
+      $(this).children('textArea').val(savedToDo);
+    }
+  })
 
   //* current Date in header
 
